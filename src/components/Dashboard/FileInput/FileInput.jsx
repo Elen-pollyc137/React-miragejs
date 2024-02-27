a29efd33483844ee7185309126920d2993c0b641
Polly <poliana_elen@hotmail.com>
2024-02-27 20:09:35 -0300

    Add: Inserindo FileInput

---
+++ src/components/Dashboard/FileInput/FileInput.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles.module.scss'
+
+export default function FileInput() {
+  return (
+    <section className={styles.container}>
+      <div className={styles.box}></div>
+    </section>
+  )
+}
