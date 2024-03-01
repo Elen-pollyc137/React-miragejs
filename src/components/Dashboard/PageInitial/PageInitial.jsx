fa4b2a599121379ad516511e43c54e838f4f8415
Polly <poliana_elen@hotmail.com>
2024-02-29 22:34:07 -0300

    Add: Inserindo FileInput

--- src/components/Dashboard/PageInitial/PageInitial.jsx
+++ src/components/Dashboard/PageInitial/PageInitial.jsx
@@ -3,7 +3,12 @@ import styles from '../styles.module.scss'
 export default function PageInitial() {
   return (
     <section className={styles.container}>
-      <div className={styles.box}></div>
+      <div className={styles.box}>
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
     </section>
   )
 }
